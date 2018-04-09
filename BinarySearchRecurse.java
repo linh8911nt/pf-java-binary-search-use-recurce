cde76431acc9053d0d8e7311d1769338edc6fd36
nguyenthanhlinh <thanhlinh12314@gmail.com>
2018-04-09 11:15:51 +0700

    binary search use recurse

---
+++ BinarySearchRecurse.java
@@ -0,0 +1,25 @@
+public class BinarySearchRecurse {
+    static int[] list = {2, 4, 7, 23, 45, 56, 73, 83, 86, 90, 93, 96, 98};
+
+    static int binarySearchRecurse(int[] list, int key, int low, int hight){
+        if (hight >= low) {
+            int mid = (low + hight) / 2;
+            if (key == list[mid]){
+                return mid;
+            }
+            if (key > list[mid]){
+                return binarySearchRecurse(list, key, mid + 1, hight);
+            }
+            if (key < list[mid]){
+                return binarySearchRecurse(list, key, low, mid - 1);
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int low = 0;
+        int hight = list.length - 1;
+        System.out.println("index is: " + binarySearchRecurse(list, 7, low, hight));
+    }
+}
